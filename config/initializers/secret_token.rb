1fc656d00c23047ab9073b619b03824907354cf2
Ghanshyam Rathod <ghanshyam.d.rathod@gmail.com>
2012-10-27 11:06:45 +0530

    [ADD] first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SdrData::Application.config.secret_token = '416d5ae554265b881d8a01c531e8a18c4766fdbf68c5f08a5c177bb181ff9e10ad5aea7fa0c54e1272cebbcff2bdcf1ec3a790f108a7cf882b66dcf29111054c'
